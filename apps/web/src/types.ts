9eb9c1470f5dc44064ff4110ff05cdfc4964b172
deep coder <43486287+code-washing@users.noreply.github.com>
2025-03-15 03:08:38 +0900

    Update types.ts

--- apps/web/src/types.ts
+++ apps/web/src/types.ts
@@ -6,7 +6,7 @@ export type ReactTag = keyof JSX.IntrinsicElements;
 export type PropsOf<TTag extends ReactTag> = TTag extends React.ElementType
   ? React.ComponentProps<TTag>
   : never;
-
+ 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
